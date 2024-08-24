28a9c486e451435720bea7c7ab2056c249186ef1
Lee Harkness <lee@leeharkness.com>
2024-08-23 20:16:02 -0500

    Added picocli

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,17 +1,36 @@
 package ${package};
 
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine.Command;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App {
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-	public static void main(String[] args) throws Exception {
-		new App().proceed();
-	}
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-	public void proceed() {
-		LOGGER.info("Hello World!");
-	}
+import java.util.concurrent.Callable;
+
+@Command(name = "helloworld", mixinStandardHelpOptions = true, version = "helloworld 1.0",
+         description = "Prints Hello World and optional text to STDOUT.")
+class App implements Callable<Integer> {
+
+    @Parameters(index = "0", arity="0..1", description = "Optional text to display.", defaultValue="")
+    private String text;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+    	System.out.println("Hello " + text);
+    	return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
