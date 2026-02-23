04334d29749ac82948b5414acbf6524b2224458c
Lee Harkness <lee@leeharkness.com>
2026-02-23 07:41:19 -0600

    let's use slf4j in tests too

--- src/main/resources/archetype-resources/src/test/java/MyTests.java
+++ src/main/resources/archetype-resources/src/test/java/MyTests.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MyTests {
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
 
 	@Test
 	void testSomething() throws Exception {
-		LOGGER.info("Started tests.");
+		log.info("Started tests.");
 		assertTrue(false);
 	}
 }
